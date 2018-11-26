8ecaba47453bdb9b6da31aaf58c2afa62c531fdb
Kerry Abernathy <Kerrysage89@gmail.com>
2018-11-26 16:07:24 -0700

    made a splash file

---
+++ my-new-project/spindl/screens/splash.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+//login
+class splash extends React.Component {
+    state = {
+        username: '',
+        password: ''
+
+    }
+    render () {
+        return (
+            <View><Text> Hello </Text></View>
+        )
+    }
+}
+
+
+
+export default splash 
