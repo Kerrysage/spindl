f12842896e6934bbdc0645e1b6b14f3056dfd119
thatvictor7 <montoya.victor7@gmail.com>
2018-11-26 17:05:33 -0700

    renamed component

--- my-new-project/spindl/screens/questionaire.js
+++ my-new-project/spindl/screens/questionaire.js
@@ -12,23 +12,31 @@ import { WebBrowser } from 'expo';
 
 import { MonoText } from '../components/StyledText';
 
-export default class HomeScreen extends React.Component {
+export default class Questionaire extends React.Component {
   static navigationOptions = {
     header: null,
   };
 
   render() {
     return (
-      <View styles=>
+      <View styles={styles.constainer}>
         <Image
          source={require('https://placeimg.com/200/200/people')}
          fadeDuration={0}
          style={{width: 20, height: 20}}
         />
+        <Text numberOfLines={5}>
+          Name
+          Age
+          Location
+        </Text>
      <View>
     )
   }
 
-  const styles = StyleSheet.create({
-
-  })
+  {/* const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#fff',
+    }
+  }) */}
