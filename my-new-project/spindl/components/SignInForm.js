df76cc787b96a9c0467735b1c99ba4678c137409
Daniel Phonelath <daniel.phonelath@gmail.com>
2018-11-28 11:18:56 -0700

    added SignIn form component

---
+++ my-new-project/spindl/components/SignInForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Stylesheet, Text, View} from 'react-native';
+
+export default class SignInForm extends React.Component {
+    render() {
+        return(
+            <View style={Stylesheet.SignInForm}>
+            
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    SignInForm:{
+        
+    }
+})
