95429f86e1f3d78a8f492ffb6c1e62b67d17c982
Kyle Czajkowski <skiroyjenkins@gmail.com>
2018-11-29 19:59:53 -0700

    getting ready to merge

--- my-new-project/spindl/components/Questionaire.js
+++ my-new-project/spindl/components/Questionaire.js
@@ -13,6 +13,8 @@ import {
 import {WebBrowser} from 'expo';
 import {MonoText} from '../components/StyledText';
 import { withNavigation } from 'react-navigation';
+import { AsyncStorage } from "react-native"
+const jwtDecode = require('jwt-decode');
 
 // import ModalDropdown from 'react-native-modal-dropdown';
 
@@ -23,11 +25,15 @@ class QuestionaireForm extends React.Component {
     };
 
     state = {
-    
+        FoodSelected: '',
+        MoviesSelected: '',
+        IndoorSelected: '',
+        OutdoorSelected: '',
+        NightlifeSelected: '',
     };
 
     clickMe = () => {
-        // console.log(this.state.OutdoorSelected,this.state.IndoorSelected,this.state.FoodSelected,this.state.NightlifeSelected,this.state.MoviesSelected)
+        console.log(this.state)
     }
 
     async componentDidMount() {
@@ -65,7 +71,22 @@ class QuestionaireForm extends React.Component {
             nightlife: nightlife.activity,
         }, function(){
         })
+        const storage = await AsyncStorage.getItem('token')
+            .then(jwtDecode)
+            .then(token => {
+                this.setState(() => {
+                    return {token: token.id} 
+                })
+                return token.id;
+            })
+        const getUser = await fetch(`https://dream-date.herokuapp.com/users/${storage}`)
+        const user = await getUser.json()
+        this.setState({
+            user: user
+        })
 }
+
+
     dropDownMenu(activity) {
         if (activity !== undefined) {
             var activityList = activity.map((item => {
@@ -79,52 +100,52 @@ render() {
     // console.log(this.state.FoodSelected);
     return (
         <ScrollView style={styles.Questionaire}>
-            <View style={styles.headerContainer}>
-                <Image 
-                    source={{uri: 'https://placeimg.com/200/200/people'}} 
-                    fadeDuration={0} 
-                    style={styles.img}
-                />
-                <View style={styles.infoContainer}>
-                    <Text style={styles.infoText}>Name</Text>
-                    <Text style={styles.infoText}>Age</Text>
-                    <Text style={styles.infoText}>Location</Text>
-                </View>
+        <View style={styles.headerContainer}>
+            <Image 
+                source={{uri: 'https://placeimg.com/200/200/people'}} 
+                fadeDuration={0} 
+                style={styles.img}
+            />
+            <View style={styles.infoContainer}>
+                <Text style={styles.infoText}>Name: </Text>
+                <Text style={styles.infoText}>Age: </Text>
+                <Text style={styles.infoText}>Location: </Text>
             </View>
+        </View>
 
-            <Text style={styles.titles}>Favorite Type of Food?</Text>
-                <View style={styles.pickerContainer}>
-                    <Picker 
-                        selectedValue={this.state.FoodSelected} 
-                        style={styles.picker}
-                        onValueChange={(itemValue, itemIndex) => this.setState({FoodSelected: itemValue})}
-                        itemStyle={{color: "#494141", alignItems: 'center', fontSize:24 }}
-                        >   
-                        {/* <Picker.Item enabled="false" label="" value="" /> */}
-                        {this.dropDownMenu(this.state.food)}
-                    </Picker>
-                </View>
-
-            <View style={styles.titlesContainer}>
-                <Text style={styles.titles}>Favorite Genre of Movies?</Text>
-            </View>
-
+        <Text style={styles.titles}>Favorite Type of Food?</Text>
             <View style={styles.pickerContainer}>
                 <Picker 
-                    selectedValue={this.state.MovieSelected} 
+                    selectedValue={this.state.FoodSelected} 
                     style={styles.picker}
-                    onValueChange={(itemValue, itemIndex) => this.setState({MoviesSelected: itemValue})}
+                    onValueChange={(itemValue, itemIndex) => this.setState({FoodSelected: itemValue})}
                     itemStyle={{color: "#494141", alignItems: 'center', fontSize:24 }}
-                    >
-                    { this.dropDownMenu(this.state.movie) }
+                    >   
+                    {/* <Picker.Item enabled="false" label="" value="" /> */}
+                    {this.dropDownMenu(this.state.food)}
                 </Picker>
             </View>
+
+        <View style={styles.titlesContainer}>
+            <Text style={styles.titles}>Favorite Genre of Movies?</Text>
+        </View>
+
+        <View style={styles.submit}>
+            <Picker 
+                selectedValue={this.state.MovieSelected} 
+                style={styles.picker}
+                onValueChange={(itemValue, itemIndex) => this.setState({MoviesSelected: itemValue})}
+                itemStyle={{color: "#494141", alignItems: 'center', fontSize:24 }}
+                >
+                { this.dropDownMenu(this.state.movie) }
+            </Picker>
+        
+        </View>
 
             <View style={styles.titlesContainer}>
                 <Text style={styles.titles}>Favorite Indoor Activities?</Text>
             </View>
-
-            <View style={styles.pickerContainer}>
+            <View style={styles.submit}>
                 <Picker 
                     selectedValue={this.state.IndoorSelected} 
                     style={styles.picker}
@@ -138,8 +159,7 @@ render() {
             <View style={styles.titlesContainer}>
                 <Text style={styles.titles}>Favorite Outdoor Activities?</Text>
             </View>
-
-            <View style={styles.pickerContainer}>
+            <View style={styles.submit}>
                 <Picker 
                     selectedValue={this.state.OutdoorSelected} 
                     style={styles.picker}
@@ -148,24 +168,20 @@ render() {
                     >
                 { this.dropDownMenu(this.state.outdoor) }
             </Picker>
-                
-            </View>
+            
+        </View>
 
-            <View style={styles.titlesContainer}>
-                <Text style={styles.titles}>Nightlife?</Text>
-            </View >
-
-            <View style={styles.pickerContainer}>
-                <Picker 
-                    selectedValue={this.state.NightlifeSelected} 
-                    style={styles.picker}
-                    onValueChange={(itemValue, itemIndex) => this.setState({NightlifeSelected: itemValue})}
-                    itemStyle={{color: "#494141", alignItems: 'center', fontSize:24 }}
-                >
-                    { this.dropDownMenu(this.state.nightlife) }
-                </Picker>
-            </View>
-
+        <View style={styles.titlesContainer}>
+            <Text style={styles.titles}>Nightlife?</Text>
+        </View>
+            <Picker 
+                selectedValue={this.state.NightlifeSelected} 
+                style={styles.picker}
+                onValueChange={(itemValue, itemIndex) => this.setState({NightlifeSelected: itemValue})}
+                itemStyle={{color: "#494141", alignItems: 'center', fontSize:24 }}
+            >
+                { this.dropDownMenu(this.state.nightlife) }
+            </Picker>
             <TouchableOpacity style={styles.button} onPress={this.clickMe}> 
                 <Text style={styles.btnText}>Send It</Text>
             </TouchableOpacity>
@@ -223,16 +239,26 @@ const styles = StyleSheet.create({
     },
     btnText: {
         color: 'rgba(255, 255, 255, 0.8)',
-        fontSize: 16,
+        fontSize: 24,
         fontWeight: 'bold'
     },
-    pickerContainer: {
+    submit: {
         borderWidth: 3,
-        borderColor: '#ffd7d7',
+        borderColor: "#fff",
+    },
+    pickerContainer:{
+        
     },
     picker:{
         backgroundColor: '#fef4f4',
         color: '#ef508c'
+    },
+
+
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        flexDirection: 'column'
     },
     profile: {
         marginTop: 35,
@@ -256,7 +282,18 @@ const styles = StyleSheet.create({
         borderBottomWidth: 1,
         textAlign: 'center'
     },
-
+    outside: {
+        display: 'flex',
+        flexDirection: 'column'
+    },
+   
+    submitFlex: {
+        display: 'flex',
+        flexDirection: 'row'
+    },
+    contentContainer: {
+        paddingVertical: 20
+    },
 
 })
 
